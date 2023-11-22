3e09e8bb34668ef10a908caf6b64b0cc9926ab11
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2023-11-22 01:43:16 -0600

    add the logo picture, finish the navbar

--- wetube/src/components/topsearch/topsearch.js
+++ wetube/src/components/topsearch/topsearch.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./topsearch.css";
+
+import logo from "../../imgs/logo.png";
+import search from "../../imgs/search.svg";
 
 const Topsearch = () => {
-  return <div>Topsearch</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" className="logo-icon" />
+        <span>WETUBE</span>
+      </div>
+
+      <div className="icons">
+        <img src={search} alt="" className="icon" />
+
+        <div className="user">
+          <img src={logo} alt="" className="icon" />
+          <span>User name</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topsearch;
