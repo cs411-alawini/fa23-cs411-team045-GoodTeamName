a8a22d4a82908701c9dd90bfca12f17974c26f26
John Randolph <johnjr4@illinois.edu>
2023-11-29 13:39:19 -0600

    Removed videoinfo from PR

--- wetube/src/pages/videoinfo/Videoinfo.js
+++ wetube/src/pages/videoinfo/Videoinfo.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from 'react-router-dom';
 import "./videoinfo.css";
 import YouTubeEmbed from "../../components/thumbnail/Thumbnail";
 
+/* Video object structure
+{
+  "videoID": string,
+  "videoTitle": string,
+  "videoCategory": string (I think),
+  "videoView":int,
+  "videoLikes":int,
+  "channel": string
+}
+
+*/
 
 const Videoinfo = () => {
   const { id } = useParams();
   const [video, setVideo] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
   // TODO: Remove this. Just conneciton test.
   // const [message, setMessage] = useState("");
 
@@ -15,31 +28,57 @@ const Videoinfo = () => {
       fetch(`http://localhost:8080/video/${id}`)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          setVideo(data);
+          // console.log(data);
+          setLoading(false);
+          if (data.length === 0) {
+            // Video not found
+            setFailed(true);
+          } else {
+            setVideo(data[0]);
+          }
         });
     } catch (err) {
       console.error(err);
+      setLoading(false);
+      setFailed(true);
     }
   },[id]);
+
+  // TODO: Replace with standardized loading graphic
+  if (loading) {
+    return (
+      <div>
+        <h2>
+          Video loading...
+        </h2>
+      </div>
+    )
+  }
+
+  // TODO: replace with standardized failure page
+  if (failed) { 
+    return (
+      <div>
+        <h2>
+          Video not found :(
+        </h2>
+      </div>
+    )
+  }
 
   return (
     <div>
       <h2>
-        Video {id}
+        {video.videoTitle}
       </h2>
-      <div>
-        Video is {JSON.stringify(video)}
-      </div>
       <div className="video-card">
-        <YouTubeEmbed videoId={id}/>
+        <YouTubeEmbed videoId={videoId}/>
       </div>
       <ul>
-        <li>Channel: placeholder</li>
-        <li>Views: Placeholder</li>
-        <li>Likes: Placeholder</li>
-        <li>Upload Date: Placeholder</li>
-        <li>Category: Placeholder</li>
+        <li>Channel: {video.channel}</li>
+        <li>Views: {video.videoView}</li>
+        <li>Likes: {video.videoLikes}</li>
+        <li>Category: {video.videoCategory}</li>
       </ul>
     </div>
   );
