bedba96d81266ff1aebee70c60d74b9cc36448e6
John Randolph <johnjr4@illinois.edu>
2023-11-29 13:26:25 -0600

    Remove logging

--- wetube/server/routes/video.js
+++ wetube/server/routes/video.js
@@ -4,8 +4,6 @@ const connection = require('../connection');
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
-    console.log("LOGGING");
-    console.log(connection);
     let sql = `SELECT * FROM Video WHERE videoID="${req.params.id}";`;
 
     connection.query(sql, function(err, result) {
@@ -13,8 +11,6 @@ router.get('/:id', (req, res) => {
             res.send(err)
             return;
         }
-        console.log(result);
-        // res.redirect('/success');
         res.json(result);
     });
 });
