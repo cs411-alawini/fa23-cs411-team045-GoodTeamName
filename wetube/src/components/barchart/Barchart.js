a7c5e7ec49b9bef67d917787b7630f94dd38f26b
Davidlasky <davidjiangxc@gmail.com>
2023-12-03 11:54:27 -0600

    completely rewrite the structure

--- wetube/src/components/barchart/Barchart.js
+++ wetube/src/components/barchart/Barchart.js
@@ -6,8 +6,8 @@ export default function Bar() {
     <BarChart
       xAxis={[{ scaleType: "band", data: ["group A", "group B", "group C"] }]}
       series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-      width={500}
-      height={300}
+      width={400}
+      height={200}
     />
   );
 }
