e09db29110d9486290f58447005b6e0ac76bd68b
John Randolph <johnjr4@illinois.edu>
2023-12-05 00:37:24 -0600

    removed video route from PR

--- wetube/server/routes/video.js
+++ wetube/server/routes/video.js
@@ -3,6 +3,7 @@ const connection = require("../connection");
 
 const router = express.Router();
 
+// to-do: get the video using the videoID
 router.get("/:id", (req, res) => {
   let sql = `SELECT * FROM Video WHERE videoID="${req.params.id}";`;
 
@@ -15,9 +16,9 @@ router.get("/:id", (req, res) => {
   });
 });
 
-// Get all the playlist ID's that this video belongs to
-router.get("/:id/p", (req, res) => {
-  let sql = `SELECT playListID FROM Contain WHERE videoID="${req.params.id}";`;
+// to-do: search for videos using the search term(video title)
+router.get("/search/:term", (req, res) => {
+  let sql = `SELECT * FROM Video WHERE videoTitle LIKE "%${req.params.term}%"  ORDER BY videoLikes, videoView ASC LIMIT 20;`;
 
   connection.query(sql, function (err, result) {
     if (err) {
@@ -25,6 +26,7 @@ router.get("/:id/p", (req, res) => {
       return;
     }
     res.json(result);
+    console.log(result);
   });
 });
 
